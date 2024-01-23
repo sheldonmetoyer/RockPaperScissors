f55c93383483289e7c4744f4d2c86c65dcb83f94
sheldonmetoyer <sheldon.metoyer@gmail.com>
2024-01-23 10:48:58 -0600

    Added the first function
    this functions grabs the computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello from JavaScript!");
+function getComputerChoice() {
+    let computerChoice = ["Rock", "Paper", "Scissors"];
+
+    return computerChoice[Math.floor(Math.random() * computerChoice.length)];
+}
+
